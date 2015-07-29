95507db27752adb0c92a47f583c4482419d03037
Jordan Klassen <jordan@klassen.me.uk>
2015-07-28 19:15:32 -0700

    allow using a row layout instead of columns

--- src/SplitPane.js
+++ src/SplitPane.js
@@ -3,6 +3,7 @@
 var React = require('react/addons');
 
 var cloneWithProps = React.addons.cloneWithProps;
+var classNames = require('classnames');
 
 /**
  * Creates a left-right split pane inside its container.
@@ -10,16 +11,18 @@ var cloneWithProps = React.addons.cloneWithProps;
 var SplitPane = React.createClass({
   getInitialState: function() {
     return {
-      dividerPosition: 50
+      dividerPosition: 50,
+      horizResize: true
     };
   },
 
   _onMouseDown: function() {
     var width = global.innerWidth;
-    global.document.body.style.cursor = 'col-resize';
+    var height = global.innerHeight;
+    global.document.body.style.cursor = this.state.horizResize ? 'col-resize' : 'row-resize';
     var moveHandler = function(event) {
       event.preventDefault();
-      this.setState({dividerPosition: (event.pageX / width) * 100});
+      this.setState({dividerPosition: (this.state.horizResize ? (event.pageX / width) : (event.pageY / height)) * 100});
     }.bind(this);
     var upHandler = function() {
       document.removeEventListener('mousemove', moveHandler);
@@ -35,44 +38,81 @@ var SplitPane = React.createClass({
     document.addEventListener('mouseup', upHandler);
   },
 
+  _onDoubleClick: function() {
+    this.setState({horizResize: !this.state.horizResize});
+  },
+
   render: function() {
     var dividerPos = this.state.dividerPosition;
-    var baseStyle = {
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      boxSizing: 'border-box'
-    };
-    var styleLeft = {
-      ...baseStyle,
-      left: 0,
-      width: dividerPos + '%',
-      paddingRight: 3
-    };
-    var styleRight = {
-      ...baseStyle,
-      right: 0,
-      width: (100 - dividerPos) + '%',
-      paddingLeft: 3
-    };
+    if (this.state.horizResize) {
+      var baseStyle = {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        boxSizing: 'border-box'
+      };
+      var styleFirst = {
+        ...baseStyle,
+        left: 0,
+        width: dividerPos + '%',
+        paddingRight: 3
+      };
+      var styleSecond = {
+        ...baseStyle,
+        right: 0,
+        width: (100 - dividerPos) + '%',
+        paddingLeft: 3
+      };
+      var dividerStyle = {
+        ...baseStyle,
+        left: dividerPos + '%',
+        width: 5,
+        marginLeft: -2.5,
+        zIndex: 100
+      };
+    } else {
+      var baseStyle = {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        boxSizing: 'border-box'
+      };
+      var styleFirst = {
+        ...baseStyle,
+        top: 0,
+        height: dividerPos + '%',
+        paddingBottom: 3
+      };
+      var styleSecond = {
+        ...baseStyle,
+        bottom: 0,
+        height: (100 - dividerPos) + '%',
+        paddingTop: 3
+      };
+      var dividerStyle = {
+        ...baseStyle,
+        top: dividerPos + '%',
+        height: 5,
+        marginTop: -2.5,
+        zIndex: 100
+      };
+    }
 
     return (
       <div className={this.props.className}>
-        <div style={styleLeft}>
+        <div style={styleFirst}>
           {this.props.children[0]}
         </div>
         <div
-          className="splitpane-divider"
+          className={classNames("splitpane-divider", {
+            "splitpane-divider-col": this.state.horizResize,
+            "splitpane-divider-row": !this.state.horizResize
+          })}
           onMouseDown={this._onMouseDown}
-          style={{
-            ...baseStyle,
-           left: dividerPos + '%',
-           width: 5,
-           marginLeft: -2.5,
-           zIndex: 100
-          }}
+          onDoubleClick={this._onDoubleClick}
+          style={dividerStyle}
         />
-        <div style={styleRight}>
+        <div style={styleSecond}>
           {this.props.children[1]}
         </div>
       </div>
