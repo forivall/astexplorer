eb8e457d8e484835b1de396d9f7d635d0c4f0fb1
Jordan Klassen <jordan@klassen.me.uk>
2015-10-09 10:50:10 -0700

    Filter out keys

--- src/PropertyList.js
+++ src/PropertyList.js
@@ -1,5 +1,40 @@
 import Element from './Element';
 import React from 'react';
+import keysIn from 'lodash.keysin';
+import isFunction from 'lodash.isfunction';
+import isUndefined from 'lodash.isundefined';
+import uniq from 'lodash.uniq';
+
+// we don't want to touch getters twice, so we don't just filter the keys,
+// we return an entire temporary object of the keys we want to display
+function getMappableProperties(o) {
+  let realKeys = keysIn(o);
+  let keys = uniq(realKeys.concat(["loc", "range"]));
+  let temp = {};
+  for (let key of (keys: Array)) {
+    let value = o[key];
+    if (!isFunction(value)) {
+      temp[key] = value;
+    }
+  }
+  // filter out duplicate private values
+  let ret = {};
+  for (let key of (keys: Array)) {
+    if (key[0] === '_') {
+      let nonPrivKey = key.slice(1), nonPrivValue;
+      if (nonPrivKey in temp) {
+        if (temp[nonPrivKey] === temp[key]) continue;
+      } else if (!isUndefined(nonPrivValue = o[nonPrivKey])) {
+        ret[nonPrivKey] = nonPrivValue;
+        continue;
+      }
+    }
+    let value = temp[key];
+    if (isUndefined(value) && realKeys.indexOf(key) === -1) continue;
+    ret[key] = value;
+  }
+  return ret;
+}
 
 export default class PropertyList {
   static defaultProps = {
@@ -10,9 +45,9 @@ export default class PropertyList {
   render() {
     var focusPath = this.props.focusPath;
     var level = this.props.level;
-
-    var properties = Object.keys(this.props.object).map(key => {
-      var v = this.props.object[key];
+    var object = getMappableProperties(this.props.object);
+    var properties = Object.keys(object).map(key => {
+      var v = object[key];
       return ( // eslint-disable-line consistent-return
         <Element
           key={key}
