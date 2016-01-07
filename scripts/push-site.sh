c8a78be1fd45e3e52378b0dbb41ca7485664ff8e
Jordan Klassen <jordan@klassen.me.uk>
2016-01-07 14:17:18 -0800

    Update rm in script

--- scripts/push-site.sh
+++ scripts/push-site.sh
@@ -23,7 +23,7 @@ cd "$TARGETPATH"
 git checkout --detach --quiet
 git branch -D gh-pages
 git checkout --orphan gh-pages
-rm -rf * .*
+rm -rf * .gitignore .eslintrc
 cd - > /dev/null
 
 echo "Building..."
