3aff15c861dffdddaa29354981ddb39c0d2ac117
Jordan Klassen <jordan@klassen.me.uk>
2016-01-07 14:02:26 -0800

    Update push-site script to include branch name in message

--- scripts/push-site.sh
+++ scripts/push-site.sh
@@ -2,6 +2,7 @@
 
 TARGETPATH="../$(basename "$(pwd)")_gh_pages"
 REMOTE=$(git remote -v | grep forivall | grep "(push)" | cut -f 2 | cut -d ' ' -f 1)
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 if ! git diff --quiet && git diff --cached --quiet; then
   echo >&2 "Cannot build, your index contains uncommitted changes."
@@ -39,7 +40,7 @@ if git diff --quiet && git diff --cached --quite; then
 fi
 echo "Committing..."
 git add -A
-git commit -m"Update site"
+git commit -m"Update site from $BRANCH"
 echo "Pushing..."
 git push --force forivall gh-pages:gh-pages
 echo "done"
